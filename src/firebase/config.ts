f321aa881cbf2715d6acba7c63ff5f337dbbb84e
Sharif Hamad Abdallah <sharifhamadx122@gmail.com>
2026-02-22 22:34:29 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-9142749103-fc9a6",
+  "appId": "1:78693011281:web:15f197ea412a936366cd70",
+  "apiKey": "AIzaSyAItLFe3gGciwq700fqItmSgyvuV7EsZLQ",
+  "authDomain": "studio-9142749103-fc9a6.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "78693011281"
+};
